75625a74c06b8e2b9d098cc6eb10cbbf8f92093d
jxy367 <jxy367@case.edu>
2018-08-23 11:46:53 -0700

    Moved loop creation

--- MALUpdateBot.py
+++ MALUpdateBot.py
@@ -455,11 +455,10 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-    for g in client.guilds:
-        if g.id not in server_channel:
-            server_channel[g.id] = g.text_channels[0].id
 
-
+for g in client.guilds:
+    if g.id not in server_channel:
+        server_channel[g.id] = g.text_channels[0].id
 client.loop.create_task(background_update())
 client.loop.create_task(cooldown())
 client.run(TOKEN)
